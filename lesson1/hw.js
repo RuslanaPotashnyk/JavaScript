01f8caa312cc5f28544fbb256c04efd19afcf8aa
RuslanaPotashnyk <ruslana22042002@gmail.com>
2022-02-02 19:06:57 +0200

    Done

--- lesson1/hw.js
+++ lesson1/hw.js
@@ -1,5 +1,6 @@
 // - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 //     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
 let wordOne = 'hello';
 let wordTwo = 'owu';
 let wordThree = 'com';
@@ -52,8 +53,10 @@ document.write(xxx);
 console.log(yyy);
 alert(yyy);
 document.write(yyy);
+
 // - Переприсвоїти кожній змінній з завдання значення на довільне.
 //     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
 wordOne = 'Hi';
 wordTwo = 'I';
 wordThree = 'am';
@@ -106,4 +109,51 @@ document.write(xxx);
 console.log(yyy);
 alert(yyy);
 document.write(yyy);
+
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+
+let firstName = 'Ruslana';
+let middleName = 'Ruslanivna';
+let lastName = 'Potashnyk';
+let person = [firstName, middleName, lastName];
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+firstName = prompt('Як тебе звати?');
+middleName = prompt('По-Батькові?');
+age = prompt('Скільки тобі років?');
+alert(`Вітаю ${firstName} ${middleName}. Тобі ${age} років`);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+
+console.log(5 < 6) // 5 ? 6 -> true
+console.log(5 > 6) // 5 ? 6 -> false
+console.log(5 >= 6) // 5 ? 6 -> false
+console.log(5 === 6) // 5 ? 6 -> false
+console.log(10 === 10) // 10 ? 10 -> true
+console.log(10 >= 10) // 10 ? 10 -> true
+console.log(10 !== 10) // 10 ? 10 -> false
+console.log(10 > 10) // 10 ? 10 -> false
+console.log(10 < 10) // 10 ? 10 -> false
+console.log(123 === '123') // 123 ? '123' -> false
+console.log(123 !== '123') // 123 ? '123' -> true
+
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+
+let str = "20";
+a = 5;
+document.write(str + a + "<br/>"); // 205, тому що при додаванні стрінгові дані просто приєднюються
+document.write(str - a + "<br/>"); // 15, віднімання допускається між стрінговим і числовим типами зміних
+document.write(str * "2" + "<br/>"); //40, множення допускається між стрінговими типами зміних
+document.write(str / 2 + "<br/>"); //10, ділення допускається між стрінговим і числовим типами зміних
