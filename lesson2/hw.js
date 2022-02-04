1730ff37ad94dd915d6976d89bd8b97f26747f06
RuslanaPotashnyk <ruslana22042002@gmail.com>
2022-02-04 15:50:51 +0200

    Done

--- lesson2/hw.js
+++ lesson2/hw.js
@@ -85,3 +85,13 @@ if (Number.isInteger(year / 4)){
 } else {
     console.log('Не високосний рік');
 }
+
+// - Використовуючи конструкцію if..else, напишіть код, який запитуватиме: „Яка «офіційна» назва JavaScript?“
+// Якщо користувач вводить «ECMAScript», то показати: «Правильно!», інакше – відобразити: «Не знаєте? ECMAScript!
+
+let officialName = prompt('Яка «офіційна» назва JavaScript?');
+if (officialName === 'ECMAScript') {
+    console.log('Правильно!');
+} else {
+    console.log('Не знаєте? ECMAScript!');
+}
