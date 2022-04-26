00143a0a36c759387e229f4a0b7464e1651b4196
RuslanaPotashnyk <ruslana22042002@gmail.com>
2022-04-26 20:22:06 +0300

    Done

---
+++ lesson1/errors.js
@@ -0,0 +1,15 @@
+let a = 100;
+let b = 500;
+let c = "hello";
+let d = "okten";
+const x = 'constant value';
+let y = 100;
+{
+    console.log(a);
+    let x = 'new value';
+}
+ b = 'new value';
+
+let result = 100 + y;
+
+console.log(result);
